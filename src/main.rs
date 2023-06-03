64af0829b3c71ae13f23f7cc25123b5ae6e45447
Josh Espinoza <joshespi@gmail.com>
2023-06-03 13:24:03 -0600

    properly reading from json file

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,32 @@
-#[macro_use]
-extern crate rocket;
-
-use rocket::figment::Figment;
-use rocket::tokio::fs;
+use rocket::{get, routes};
+use rocket::serde::{Serialize, Deserialize};
 use rocket::serde::json::Json;
-use rocket::serde::{Deserialize, Serialize};
-use tokio::fs::File;
 
-#[derive(Serialize, Deserialize)]
-struct Asset {
-    symbol: String,
-    name: String,
-    quantity: f64,
-    price: f64,
-}
+#[macro_use] extern crate rocket;
 
-#[derive(Serialize, Deserialize)]
+
+#[derive(Debug, Deserialize, Serialize)]
 struct Portfolio {
-    assets: Vec<Asset>,
+    name: String,
+    holdings: Vec<Holding>,
 }
 
-impl Portfolio {
-    fn new() -> Portfolio {
-        Portfolio {
-            assets: Vec::new(),
-        }
-    }
+#[derive(Debug, Deserialize, Serialize)]
+struct Holding {
+    symbol: String,
+    quantity: u32,
 }
 
 #[get("/")]
-fn index() -> &'static str {
-    "Welcome to the portfolio tracker!"
-}
-
-#[get("/portfolio")]
-async fn get_portfolio() -> Json<Portfolio> {
-    println!("Reading portfolio file...");
-    let file_content = match fs::read_to_string("portfolio.json").await {
-        Ok(content) => content,
-        Err(_) => {
-            eprintln!("Error reading file");
-            return Json(Portfolio::new());
-        }
-    };
-
-    let portfolio: Portfolio = match serde_json::from_str(&file_content) {
-        Ok(portfolio) => portfolio,
-        Err(_) => {
-            eprintln!("Error deserializing JSON");
-            return Json(Portfolio::new());
-        }
-    };
+fn get_portfolio() -> Json<Portfolio> {
+    // Read the portfolio data from a JSON file
+    let portfolio: Portfolio = serde_json::from_str(include_str!("portfolio.json"))
+        .expect("Failed to read portfolio data");
 
     Json(portfolio)
 }
 
-
-#[rocket::main]
-async fn main() {
-    let figment = Figment::from(rocket::Config::default());
-    let rocket = rocket::custom(figment)
-        .mount("/", routes![index, get_portfolio]);
-
-    rocket.launch().await.unwrap();
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![get_portfolio])
 }
